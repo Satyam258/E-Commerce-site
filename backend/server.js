c64eb3d213c68b9bf652ea164c18449418bc7137
Satyam Singh <satyamsinghksh@gmail.com>
2025-07-27 13:53:25 +0530

    Add user authentication and registration functionality with JWT

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db.js");
+const userRoutes = require("./routes/userRoutes.js")
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = 9000;
+dotenv.config();
+
+
+const PORT = process.env.PORT || 3000;
+
+//Connect to MongoDB
+
+connectDB();
 
 app.get("/",(req,res)=>{
     res.send("WELCOME TO RABBIT API!");
 });
+
+// API Routes
+
+app.use("/api/users",userRoutes);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
