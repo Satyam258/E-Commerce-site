f1a2851b3e1929d44e50ed540bd0246d7d5860f1
Satyam Singh <satyamsinghksh@gmail.com>
2025-08-04 22:43:56 +0530

    feat: Implement checkout functionality with routes for creating, updating, and finalizing checkouts; add Checkout and Order models

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db.js");
 const userRoutes = require("./routes/userRoutes.js")
 const productRoutes = require("./routes/productRoutes.js")
 const cartRoutes = require("./routes/cartRoutes.js")
+const checkoutRoutes = require("./routes/checkoutRoutes.js")
 
 const app = express();
 app.use(express.json());
@@ -28,6 +29,7 @@ app.get("/",(req,res)=>{
 app.use("/api/users",userRoutes);
 app.use("/api/products",productRoutes);
 app.use("/api/cart",cartRoutes);
+app.use("/api/checkout",checkoutRoutes);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
